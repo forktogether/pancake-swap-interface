18271317eaa105fc243941f2c55c0799a0175d0e
ChefTula <82691742+ChefTula@users.noreply.github.com>
2021-04-18 23:13:05 +0000

    Update index.ts

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -3,7 +3,7 @@ import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
-  [ChainId.BSCTESTNET]: '0xC7c137ce164FdAdaf4AA4D78254A0e2CD4a450ed'
+  [ChainId.BSCTESTNET]: '0xc61e24Ed18f07AaDBbFBf170Adada36769d7b76d'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
