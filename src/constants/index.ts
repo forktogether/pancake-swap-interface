91a3a781e5dfa1cd2c0f67744f534e40b8656889
ChefTula <82691742+ChefTula@users.noreply.github.com>
2021-04-20 17:58:11 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,5 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
 
-export enum ChainId {
-  MAINNET = 56,
-  BSCTESTNET = 97,
-  KOVAN 42
-}
-
 export const ROUTER_ADDRESS = '0xe45a489d040Da08FBc0ebF4F976fDA6b8cA73DD2'
 
 // a list of tokens by chain
@@ -13,7 +7,7 @@ type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
-export const CAKE = new Token(ChainId.KOVAN, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'CAKE', 'PancakeSwap Token')
+export const CAKE = new Token(42.KOVAN, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'CAKE', 'PancakeSwap Token')
 export const WBNB = new Token(ChainId.KOVAN, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.KOVAN, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.KOVAN, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
