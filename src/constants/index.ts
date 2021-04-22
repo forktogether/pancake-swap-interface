6a78384ee44f1937ad6756226a20b73b5a1d4db1
ChefTula <82691742+ChefTula@users.noreply.github.com>
2021-04-22 22:05:05 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,6 +7,7 @@ type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
+export const MILKY = new Token(ChainId.BSCTESTNET, '0x86bD1A898C2671E5a8d9c8fBb6Cf5d40B424e1fE', 18, 'MILKY', 'Milky Token')
 export const CAKE = new Token(ChainId.MAINNET, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'CAKE', 'PancakeSwap Token')
 export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
