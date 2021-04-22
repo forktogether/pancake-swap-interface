64a7b99980d3fa36eb419c90f23742e6987a09f6
ChefTula <82691742+ChefTula@users.noreply.github.com>
2021-04-22 02:52:14 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,8 +6,9 @@ export const ROUTER_ADDRESS = '0xDF5F0101CACb6A635e4be7B5b47DD6cd3851CA92'
 type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
+export const MILK = new Token(ChainId.BSCTESTNET, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'MILK', 'MilkySwap Token')
 
-export const CAKE = new Token(ChainId.MAINNET, '0x6562BF7d4898A4CFCc5Bdc39a9045fc81Ee920a8', 18, 'MILKY', 'MilkySwap Token')
+export const CAKE = new Token(ChainId.MAINNET, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'CAKE', 'PancakeSwap Token')
 export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.MAINNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
