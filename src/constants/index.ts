d16ad713d6a0e2a37b204f45218feae5c8b6c4f1
ChefTula <82691742+ChefTula@users.noreply.github.com>
2021-04-22 17:53:41 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,7 +9,7 @@ type ChainTokenList = {
 export const MILK = new Token(ChainId.BSCTESTNET, '0x6562BF7d4898A4CFCc5Bdc39a9045fc81Ee920a8', 18, 'MILK', 'MilkySwap Token')
 
 export const CAKE = new Token(ChainId.BSCTESTNET, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'CAKE', 'PancakeSwap Token')
-export const WBNB = new Token(ChainId.BSCTESTNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
+export const WBNB = new Token(ChainId.BSCTESTNET, '0x57bDE84aC6B6a950787964cb1c7536b9fef02E83', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.BSCTESTNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.BSCTESTNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
 export const BTCB = new Token(ChainId.BSCTESTNET, '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', 18, 'BTCB', 'Binance BTC')
