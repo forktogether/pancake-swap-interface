f950e181eeadaeee59edd64745d2ed7e176147e3
ChefTula <82691742+ChefTula@users.noreply.github.com>
2021-04-22 02:10:51 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,13 @@
 import { ChianId, JSBI, Percent, Token, WETH } from '@pancake-swap-lib/sdk'
 
-export const ROUTER_ADDRESS = '0xe45a489d040Da08FBc0ebF4F976fDA6b8cA73DD2'
+export const ROUTER_ADDRESS = '0xDF5F0101CACb6A635e4be7B5b47DD6cd3851CA92'
 
 // a list of tokens by chain
 type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
-export const CAKE = new Token(ChainId.BSCTESTNET, '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82', 18, 'CAKE', 'PancakeSwap Token')
+export const CAKE = new Token(ChainId.BSCTESTNET, '0x6562BF7d4898A4CFCc5Bdc39a9045fc81Ee920a8', 18, 'CAKE', 'PancakeSwap Token')
 export const WBNB = new Token(ChainId.BSCTESTNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.BSCTESTNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.BSCTESTNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
